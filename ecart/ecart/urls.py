d7b043cb6ad9bc746a7eac8f4dd41e0b73b80c1e
favaskri <72196438+favaskri@users.noreply.github.com>
2024-10-15 11:29:06 +0530

    cart page compleat

--- ecart/ecart/urls.py
+++ ecart/ecart/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('product.urls')),
-    path('account/',include('customers.urls'))
+    path('account/',include('customers.urls')),
+    path('orders/',include('orders.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
