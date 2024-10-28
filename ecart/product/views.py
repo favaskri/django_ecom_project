213eb05fbb574aae9f7b335b662aea51ee749717
favaskri <favas13270@gmail.com>
2024-10-28 09:36:05 +0530

    'Featured products updated in home page"

--- ecart/product/views.py
+++ ecart/product/views.py
@@ -7,7 +7,15 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    featured_products =Product.objects.order_by('priority')[:4]
+    latest_products =Product.objects.order_by('-id')[:4]
+    context={
+        'featured_products':featured_products,
+        'latest_products':latest_products
+    }
+
+
+    return render(request,'index.html',context)
 
 def product_lists(request):
     page=1
